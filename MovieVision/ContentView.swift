6e2316f97eab1eb15a046a3fc96a4c0e2c40caf3
Julio César Fernández <juliocesar.fernandez@gabhel.com>
2024-01-15 18:48:29 +0100

    Initial Commit

---
+++ MovieVision/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MovieVision
+//
+//  Created by Julio César Fernández Muñoz on 15/1/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ MovieVision/MovieVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieVisionApp.swift
+//  MovieVision
+//
+//  Created by Julio César Fernández Muñoz on 15/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
